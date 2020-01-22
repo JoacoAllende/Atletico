04df4d83b3f375f7dfb6797958762565402a1aa7
Joaquin Allende <joacoallendeids@gmail.com>
2020-01-22 17:49:41 -0300

    Completa escudos de los equipos

---
+++ db/inicial-2020.sql
@@ -0,0 +1,27 @@
+UPDATE equipo
+SET nombre = 'Club Deportivo El Leon (Gral. Madariaga)'
+WHERE nombre = 'Club Deportivo El Leon';
+
+UPDATE equipo
+SET nombre = 'Club Atletico Jorge Newbery (Maipu)'
+WHERE nombre = 'Club Atletico Jorge Newbery';
+
+UPDATE equipo
+SET nombre = 'Club Deportivo Juventud Unida (Gral. Madariaga)'
+WHERE nombre = 'Club Deportivo Juventud Unida' OR id = 42;
+
+UPDATE equipo
+SET nombre = 'Club Estudiantes de La Plata A.F.A.'
+WHERE nombre = 'Club Estudiantes de La Plata A.F.A';
+
+UPDATE equipo
+SET nombre = 'Club Nacional de Futbol de Uruguay'
+WHERE nombre = 'Club Nacional de Futbol (Uruguay)';
+
+UPDATE equipo
+SET nombre = 'Club Social y Deportivo El Fortin'
+WHERE id = 4 OR id = 29;
+
+UPDATE equipo
+SET nombre = 'C.A.F.A. (Moreno)'
+WHERE nombre =' C.A.F.A. (Moreno)';
