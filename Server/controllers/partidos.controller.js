b1ced13ed13b5c9f0e9cd6812bd64d7ebb07eb4b
Joaquin Allende <joacoallendeids@gmail.com>
2026-02-07 09:40:23 -0300

    Arregla orden partido

--- Server/controllers/partidos.controller.js
+++ Server/controllers/partidos.controller.js
@@ -62,7 +62,8 @@ partidosController.getCanchaInstancias = async (req, res, next) => {
 partidosController.createPartido = (req, res) => {
     const partido = req.body;
     const {to, a} = req.params;
-    const query = `INSERT INTO juega (id_equipoUno, id_equipoDos, ${partido.id_grupo !== null ? 'id_grupo,' : ''} ${partido.instancia !== null ? 'instancia,' : ''} anio, torneo, dia, cancha) VALUES (${partido.id_equipoUno},${partido.id_equipoDos},${partido.id_grupo !== null ? `${partido.id_grupo},` : ''}${partido.instancia !== null ? `"${partido.instancia}",` : ''}${a}, ${to},"${partido.dia}","${partido.cancha}");`;
+    const orden = partido.orden != null && partido.orden !== '' ? parseInt(partido.orden) : null;
+    const query = `INSERT INTO juega (id_equipoUno, id_equipoDos, ${partido.id_grupo !== null ? 'id_grupo,' : ''} ${partido.instancia !== null ? 'instancia,' : ''} anio, torneo, dia, cancha, orden) VALUES (${partido.id_equipoUno},${partido.id_equipoDos},${partido.id_grupo !== null ? `${partido.id_grupo},` : ''}${partido.instancia !== null ? `"${partido.instancia}",` : ''}${a}, ${to},"${partido.dia}","${partido.cancha}",${orden});`;
     mysqlConnection.query(query, (err) => {
         if(!err) {
             res.json({
@@ -76,7 +77,8 @@ partidosController.createPartido = (req, res) => {
 
 partidosController.updatePartido = (req, res) => {
     const partido = req.body;
-    const query = `UPDATE juega SET id_equipoUno = ${partido.id_equipoUno}, id_equipoDos = ${partido.id_equipoDos}, ${partido.id_grupo !== null ? `id_grupo = ${partido.id_grupo},` : ''} ${partido.instancia !== null ? `instancia = '${partido.instancia}',` : ''} dia = '${partido.dia}', cancha = '${partido.cancha}' WHERE id_partido = ${partido.id_partido};`
+    const orden = partido.orden != null && partido.orden !== '' ? parseInt(partido.orden) : null;
+    const query = `UPDATE juega SET id_equipoUno = ${partido.id_equipoUno}, id_equipoDos = ${partido.id_equipoDos}, ${partido.id_grupo !== null ? `id_grupo = ${partido.id_grupo},` : ''} ${partido.instancia !== null ? `instancia = '${partido.instancia}',` : ''} dia = '${partido.dia}', cancha = '${partido.cancha}', orden = ${orden} WHERE id_partido = ${partido.id_partido};`
     mysqlConnection.query(query, (err) => {
         if (!err) {
             res.json({
